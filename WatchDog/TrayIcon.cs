d1b9aad547fb8d09ff98f26d9a7f334744da908e
bxbsean <sean@bitbybitstudios.com>
2023-11-21 15:27:25 -0800

    Out of range index error fixes, shutdown fix
    Fully terminate process when user elects to do so via tray icon.
    Prevent out of range indexing when manipulating application list in config form.
    When user cancels adding an item to the config, do not add the empty item to the config list.

--- WatchDog/TrayIcon.cs
+++ WatchDog/TrayIcon.cs
@@ -10,236 +10,236 @@ using WatchdogLib;
 
 namespace WatchDog
 {
-    namespace TrayIconTest
-    {
-        class TrayIcon : ApplicationContext
-        {
-            private bool _disposed;
+	namespace TrayIconTest
+	{
+		internal class TrayIcon : ApplicationContext
+		{
+			private bool _disposed;
 
-            //Component declarations
-            private NotifyIcon _trayIcon;
-            private LogForm _logForm;
-            private ContextMenuStrip _trayIconContextMenu;
-            private ToolStripMenuItem _showLogMenuItem;
-            private ToolStripMenuItem _closeMenuItem;
-            private Logger _logger;
-            private MainForm  _mainForm;
-            private Configuration _configuration;
-            private ConfigurationSerializer<Configuration> _configurationSerializer;
+			//Component declarations
+			private NotifyIcon _trayIcon;
+			private LogForm _logForm;
+			private ContextMenuStrip _trayIconContextMenu;
+			private ToolStripMenuItem _showLogMenuItem;
+			private ToolStripMenuItem _closeMenuItem;
+			private Logger _logger;
+			private MainForm _mainForm;
+			private Configuration _configuration;
+			private ConfigurationSerializer<Configuration> _configurationSerializer;
 
-            public TrayIcon()
-            {
-                Application.ApplicationExit += OnApplicationExit;
-                InitializeComponent();
+			public TrayIcon()
+			{
+				Application.ApplicationExit += OnApplicationExit;
+				InitializeComponent();
 
-                _configuration = new Configuration();
-                _configurationSerializer = new Utilities.ConfigurationSerializer<Configuration>("configuration.json",_configuration);
-                _configuration =  _configurationSerializer.Deserialize();
-
-
-                // Todo visibility dependent on configuration. If not, only show config form on 2nd startup
-
-                _logForm = new LogForm() {Visible = false };
-                _mainForm = new MainForm();
-
-                _trayIcon.Visible = _configuration.ShowTrayIcon;
-                InitializeApplication();
-
-                //Debug.WriteLine(Reboot.GetUptime());
-                //Debug.WriteLine(Reboot.GetUptime2());
-                //Reboot.Shutdown(ShutdownType.Reboot, ForceExit.Force,ShutdownReason.MinorOther);
-            }
-
-            ~TrayIcon()
-            {
-                if (_trayIcon != null)
-                {
-                    _trayIcon.Visible = false;
-                }
-            }
-
-            protected override void Dispose(bool disposing)
-            {
-                if (!_disposed)
-                {
-                    if (disposing)
-                    {
-                        // free other managed objects that implement
-                        if (_trayIcon != null)
-                        {
-                            _trayIcon.Visible = false;
-                            _trayIcon.Dispose();
-                            _trayIcon = null;
-                        }
-                    }
-
-                    // release any unmanaged objects
-                    // set object references to null
-
-                    _disposed = true;
-                }
-
-                base.Dispose(disposing);
-            }
+				_configuration = new Configuration();
+				_configurationSerializer = new Utilities.ConfigurationSerializer<Configuration>("configuration.json", _configuration);
+				_configuration = _configurationSerializer.Deserialize();
 
 
-            private void InitializeComponent()
-            {
-                try
-                {
-                    var path = Path.GetDirectoryName(Application.ExecutablePath);
-                    if (!String.IsNullOrEmpty(path) && Directory.Exists(path))
-                        Directory.SetCurrentDirectory(path);
+				// Todo visibility dependent on configuration. If not, only show config form on 2nd startup
 
-                    _logger = LogManager.GetLogger("WatchdogServer");
-                    ExceptionsManager.Logger = _logger;
+				_logForm = new LogForm() { Visible = false };
+				_mainForm = new MainForm();
 
-                    _trayIcon = new NotifyIcon();
-                    ExceptionsManager.TrayIcon = _trayIcon;
-                    _trayIcon.Text = "Watchdog";                    
-                    _trayIcon.BalloonTipIcon = ToolTipIcon.Info;
-                    _trayIcon.BalloonTipText = "Starting watchdog";
-                    _trayIcon.BalloonTipTitle = "Starting watchdog";
-                    _trayIcon.Text = "Watchdog";
+				_trayIcon.Visible = _configuration.ShowTrayIcon;
+				InitializeApplication();
 
-                    //The icon is added to the project resources.
-                    _trayIcon.Icon = Resources.watchdog;
+				//Debug.WriteLine(Reboot.GetUptime());
+				//Debug.WriteLine(Reboot.GetUptime2());
+				//Reboot.Shutdown(ShutdownType.Reboot, ForceExit.Force,ShutdownReason.MinorOther);
+			}
 
-                    //Optional - handle double-clicks on the icon:
-                    _trayIcon.Click += TrayIconClick;
-                    _trayIcon.DoubleClick += TrayIconDoubleClick;
+			~TrayIcon()
+			{
+				if (_trayIcon != null)
+				{
+					_trayIcon.Visible = false;
+				}
+			}
 
-                    //Optional - Add a context menu to the TrayIcon:
-                    _trayIconContextMenu = new ContextMenuStrip();
+			protected override void Dispose(bool disposing)
+			{
+				if (!_disposed)
+				{
+					if (disposing)
+					{
+						// free other managed objects that implement
+						if (_trayIcon != null)
+						{
+							_trayIcon.Visible = false;
+							_trayIcon.Dispose();
+							_trayIcon = null;
+						}
+					}
 
-                    _trayIconContextMenu.SuspendLayout();
-                                    
-                    _trayIconContextMenu.Name = "_trayIconContextMenu";
-                    _trayIconContextMenu.Size = new Size(153, 70);
+					// release any unmanaged objects
+					// set object references to null
 
-                    // Show log
-                    _showLogMenuItem = new ToolStripMenuItem
-                    {
-                        Name = "_showLogMenuItem",
-                        Size = new Size(152, 22),
-                        Text = "Show watchdog log"
-                    };
-                    _showLogMenuItem.Click += ShowLogMenuItemClick;
-                    _trayIconContextMenu.Items.AddRange(new ToolStripItem[] { _showLogMenuItem });
+					_disposed = true;
+				}
 
-                    // CloseMenuItem
-                    _closeMenuItem = new ToolStripMenuItem
-                    {
-                        Name = "_suspendResumeMenuItem",
-                        Size = new Size(152, 22),
-                        Text = "Suspend watchdog server"
-                    };
-                    _closeMenuItem.Click += CloseMenuItemClick;
-                    _trayIconContextMenu.Items.AddRange(new ToolStripItem[] { _closeMenuItem });
+				base.Dispose(disposing);
+			}
 
 
+			private void InitializeComponent()
+			{
+				try
+				{
+					var path = Path.GetDirectoryName(Application.ExecutablePath);
+					if (!string.IsNullOrEmpty(path) && Directory.Exists(path))
+						Directory.SetCurrentDirectory(path);
 
-                    _trayIconContextMenu.ResumeLayout(false);
-                    _trayIcon.ContextMenuStrip = _trayIconContextMenu;
-                }
-                catch (Exception ex)
-                {
-                    ExceptionsManager.ServerCrash("Exception Watchdog initialization", "Exception during Watchdog initialization :" + ex.Message, true);
-                }
-            }
+					_logger = LogManager.GetLogger("WatchdogServer");
+					ExceptionsManager.Logger = _logger;
 
-            private void ShowLogMenuItemClick(object sender, EventArgs e)
-            {
-                _logForm.Visible = true;
-            }
+					_trayIcon = new NotifyIcon();
+					ExceptionsManager.TrayIcon = _trayIcon;
+					_trayIcon.Text = "Watchdog";
+					_trayIcon.BalloonTipIcon = ToolTipIcon.Info;
+					_trayIcon.BalloonTipText = "Starting watchdog";
+					_trayIcon.BalloonTipTitle = "Starting watchdog";
+					_trayIcon.Text = "Watchdog";
 
-            private void OnApplicationExit(object sender, EventArgs e)
-            {
-                _logger.Info("Stopping the watchdog application");
+					//The icon is added to the project resources.
+					_trayIcon.Icon = Resources.watchdog;
 
-                if (_trayIcon != null) _trayIcon.Visible = false;
+					//Optional - handle double-clicks on the icon:
+					_trayIcon.Click += TrayIconClick;
+					_trayIcon.DoubleClick += TrayIconDoubleClick;
 
+					//Optional - Add a context menu to the TrayIcon:
+					_trayIconContextMenu = new ContextMenuStrip();
 
-            }
+					_trayIconContextMenu.SuspendLayout();
 
-            private void TrayIconClick(object sender, EventArgs e)
-            {
-                var me = (MouseEventArgs)e;
-                if (me.Button == System.Windows.Forms.MouseButtons.Left)
-                {
-                    //_logForm.Visible = true;
-                    _mainForm.Visible = true;
-                }
-            }
+					_trayIconContextMenu.Name = "_trayIconContextMenu";
+					_trayIconContextMenu.Size = new Size(153, 70);
 
+					// Show log
+					_showLogMenuItem = new ToolStripMenuItem
+					{
+						Name = "_showLogMenuItem",
+						Size = new Size(152, 22),
+						Text = "Show Log"
+					};
+					_showLogMenuItem.Click += ShowLogMenuItemClick;
+					_trayIconContextMenu.Items.AddRange(new ToolStripItem[] { _showLogMenuItem });
 
-            private void TrayIconDoubleClick(object sender, EventArgs e)
-            {
-                _logForm.Visible = true;
-            }
-
-            private void CloseMenuItemClick(object sender, EventArgs e)
-            {
-                ApplicationExit();
-            }
-
-
-            private void InitializeApplication()
-            {
-                try
-                {
-                    _logger = LogManager.GetLogger("WatchdogServer");
-                    var nlogEventTarget =  Utilities.NlogEventTarget.Instance;
-                    nlogEventTarget.OnLogEvent+= OnLogEvent;
-
-
-                    Directory.CreateDirectory(FileUtils.Combine(Path.GetDirectoryName(new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath), "\\logs"));
-
-
-                    _logger.Info("Initializing watchdog application");
-                    string[] args = Environment.GetCommandLineArgs();
-
-                    // Strip application from arguments
-                    args = args.Where(w => w != args[0]).ToArray();
-                    MainApplication(args);
-                }
-                catch (Exception ex)
-                {
-                    ExceptionsManager.ServerCrash("Exception during Watchdog initialization", "Exception during Watchdog initialization :" + ex.Message, true);
-                }
-            }
-
-
-            private void OnLogEvent(object sender, Utilities.LogEventArgs logEventArgs)
-            {
-
-                _logForm.LoggingView.AddEntry(logEventArgs.LogLines.ToArray());
-                foreach (var logLine in logEventArgs.LogLines)
-                {
-                    Debug.WriteLine(logLine);
-                }
-            }
-
-
-            private void MainApplication(string[] args)
-            {
-                ApplicationWatcher applicationWatcher = new ApplicationWatcher(_logger);
-                applicationWatcher.Deserialize(_configuration);
-                //applicationWatcher.AddMonitoredApplication("MonitoredApplication", @"D:\DevelPers\WatchDog\MonitoredApplication\bin\Release\MonitoredApplication.exe",10,10,1,1,false,true,true,true);
-                //var applicationHandler = new ApplicationHandler("MonitoredApplication", @"D:\DevelPers\WatchDog\MonitoredApplication\bin\Release\MonitoredApplication.exe", 10, 10, 1, 1,false, true) {Active = true};
-                //applicationWatcher.ApplicationHandlers.Add(applicationHandler);
-
-                new MainFormVm(_mainForm, applicationWatcher, _configuration, _configurationSerializer);
-            }
+					// CloseMenuItem
+					_closeMenuItem = new ToolStripMenuItem
+					{
+						Name = "_suspendResumeMenuItem",
+						Size = new Size(152, 22),
+						Text = "Shutdown"
+					};
+					_closeMenuItem.Click += CloseMenuItemClick;
+					_trayIconContextMenu.Items.AddRange(new ToolStripItem[] { _closeMenuItem });
 
 
 
-            private void ApplicationExit()
-            {
-                _trayIcon.Visible = false;
-                OnApplicationExit(this, null);
-            }
+					_trayIconContextMenu.ResumeLayout(false);
+					_trayIcon.ContextMenuStrip = _trayIconContextMenu;
+				}
+				catch (Exception ex)
+				{
+					ExceptionsManager.ServerCrash("Exception Watchdog initialization", "Exception during Watchdog initialization :" + ex.Message, true);
+				}
+			}
 
-        }
-    }
+			private void ShowLogMenuItemClick(object sender, EventArgs e)
+			{
+				_logForm.Visible = true;
+			}
+
+			private void OnApplicationExit(object sender, EventArgs e)
+			{
+				_logger.Info("Stopping the watchdog application");
+
+				if (_trayIcon != null) _trayIcon.Visible = false;
+
+				Application.Exit();
+			}
+
+			private void TrayIconClick(object sender, EventArgs e)
+			{
+				var me = (MouseEventArgs)e;
+				if (me.Button == System.Windows.Forms.MouseButtons.Left)
+				{
+					//_logForm.Visible = true;
+					_mainForm.Visible = true;
+				}
+			}
+
+
+			private void TrayIconDoubleClick(object sender, EventArgs e)
+			{
+				_logForm.Visible = true;
+			}
+
+			private void CloseMenuItemClick(object sender, EventArgs e)
+			{
+				ApplicationExit();
+			}
+
+
+			private void InitializeApplication()
+			{
+				try
+				{
+					_logger = LogManager.GetLogger("WatchdogServer");
+					var nlogEventTarget = Utilities.NlogEventTarget.Instance;
+					nlogEventTarget.OnLogEvent += OnLogEvent;
+
+
+					Directory.CreateDirectory(FileUtils.Combine(Path.GetDirectoryName(new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath), "\\logs"));
+
+
+					_logger.Info("Initializing watchdog application");
+					string[] args = Environment.GetCommandLineArgs();
+
+					// Strip application from arguments
+					args = args.Where(w => w != args[0]).ToArray();
+					MainApplication(args);
+				}
+				catch (Exception ex)
+				{
+					ExceptionsManager.ServerCrash("Exception during Watchdog initialization", "Exception during Watchdog initialization :" + ex.Message, true);
+				}
+			}
+
+
+			private void OnLogEvent(object sender, Utilities.LogEventArgs logEventArgs)
+			{
+
+				_logForm.LoggingView.AddEntry(logEventArgs.LogLines.ToArray());
+				foreach (var logLine in logEventArgs.LogLines)
+				{
+					Debug.WriteLine(logLine);
+				}
+			}
+
+
+			private void MainApplication(string[] args)
+			{
+				ApplicationWatcher applicationWatcher = new ApplicationWatcher(_logger);
+				applicationWatcher.Deserialize(_configuration);
+				//applicationWatcher.AddMonitoredApplication("MonitoredApplication", @"D:\DevelPers\WatchDog\MonitoredApplication\bin\Release\MonitoredApplication.exe",10,10,1,1,false,true,true,true);
+				//var applicationHandler = new ApplicationHandler("MonitoredApplication", @"D:\DevelPers\WatchDog\MonitoredApplication\bin\Release\MonitoredApplication.exe", 10, 10, 1, 1,false, true) {Active = true};
+				//applicationWatcher.ApplicationHandlers.Add(applicationHandler);
+
+				new MainFormVm(_mainForm, applicationWatcher, _configuration, _configurationSerializer);
+			}
+
+
+
+			private void ApplicationExit()
+			{
+				_trayIcon.Visible = false;
+				OnApplicationExit(this, null);
+			}
+
+		}
+	}
 }
